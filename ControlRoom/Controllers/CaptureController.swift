72c9d0ccdb3e138d29a7ce089dac03de354f93c2
Paul Hudson <paul.hudson@gmail.com>
2023-05-10 13:47:32 +0100

    Moved a lot of logic out of ControlView and into a separate controller. You're welcome, Future Paul.

---
+++ ControlRoom/Controllers/CaptureController.swift
@@ -0,0 +1,154 @@
+//
+//  CaptureController.swift
+//  ControlRoom
+//
+//  Created by Paul Hudson on 10/05/2023.
+//  Copyright © 2023 Paul Hudson. All rights reserved.
+//
+
+import SwiftUI
+
+/// Handles all screenshotting and video creation.
+class CaptureController: ObservableObject {
+    /// The user's settings for capturing
+    @AppStorage("captureSettings") var settings = CaptureSettings(imageFormat: .png, videoFormat: .h264, display: .internal, mask: .ignored)
+
+    /// The currently active recording process, if it exists. We don't need to monitor this, just keep it alive.
+    @Published var recordingProcess: Process?
+
+    /// The name of the file we're writing to, used at first in a temporary directory then on the desktop.
+    @Published var recordingFilename = ""
+
+    /// The export format description to be shown while exporting
+    @Published var exportDescription = ""
+
+    /// Converting MP4 to GIF takes time, so this tracks the progress of the operation
+    @Published var exportProgress: CGFloat = 1.0
+
+    private var videoFormat = SimCtl.IO.VideoFormat.h264
+
+    var imageFormatString: String {
+        settings.imageFormat.rawValue.uppercased()
+    }
+
+    var videoFormatString: String {
+        settings.videoFormat.name
+    }
+
+    /// Takes a screenshot of the device's current screen and saves it to the desktop.
+    func takeScreenshot(of simulator: Simulator, format: SimCtl.IO.ImageFormat? = nil) {
+        // If the user asked for a specific format then use it, otherwise
+        // use whatever is our default.
+        let resolvedFormat = format ?? settings.imageFormat
+
+        SimCtl.saveScreenshot(simulator.id, to: makeScreenshotFilename(format: resolvedFormat), type: resolvedFormat, display: settings.display, with: settings.mask)
+    }
+
+    /// Creates a filename for a screenshot that ought to be unique
+    func makeScreenshotFilename(format: SimCtl.IO.ImageFormat) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd-HH-mm-ss"
+
+        let dateString = formatter.string(from: Date.now)
+
+        return "~/Desktop/ControlRoom-\(dateString).\(format.rawValue)"
+    }
+
+    /// Starts recording video of the device, saving it to the desktop.
+    func startRecordingVideo(of simulator: Simulator, format: SimCtl.IO.VideoFormat? = nil) {
+        // Store the format we've been asked to record in, so we can export to GIF
+        // correctly later on.
+        videoFormat = format ?? settings.videoFormat
+
+        recordingFilename = makeVideoFilename()
+
+        let tempPath = FileManager.default.temporaryDirectory.appendingPathComponent(recordingFilename).path
+
+        recordingProcess = SimCtl.startVideo(simulator.id, to: tempPath, type: .h264, display: settings.display, with: settings.mask)
+    }
+
+    func stopRecordingVideo() {
+        recordingProcess?.interrupt()
+        recordingProcess?.waitUntilExit()
+        recordingProcess = nil
+
+        let sourceURL = FileManager.default.temporaryDirectory.appendingPathComponent(recordingFilename)
+
+        let paths = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask)
+        let savePath = paths[0].appendingPathComponent(recordingFilename).path
+
+        let format = videoFormat.name
+
+        if format.hasPrefix("GIF") {
+            exportGif(format, savePath, sourceURL)
+        } else if format.contains("Compressed") {
+            exportCompressedVideo(savePath, sourceURL)
+        } else {
+            try? FileManager.default.moveItem(atPath: sourceURL.path, toPath: savePath)
+        }
+    }
+
+    /// Saves recorded video as a GIF-file
+    private func exportGif(_ format: String, _ savePath: String, _ sourceURL: URL) {
+        let size: CGFloat?
+
+        if format.contains("Small") {
+            size = 400
+        } else if format.contains("Medium") {
+            size = 800
+        } else if format.contains("Large") {
+            size = 1200
+        } else {
+            size = 1600
+        }
+
+        let gifExtension = savePath.replacingOccurrences(of: ".mp4", with: ".gif")
+
+        exportDescription = "GIF"
+
+        Task {
+            let result = try await sourceURL.convertToGIF(maxSize: size) { [weak self] progress in
+                self?.exportProgress = progress
+            }
+
+            switch result {
+            case .success(let gifURL):
+                try? FileManager.default.moveItem(atPath: gifURL.path, toPath: gifExtension)
+            case .failure(let reason):
+                print(reason.localizedDescription)
+            }
+        }
+    }
+
+    /// Compresses recorded video with `ffmpeg` before saving
+    private func exportCompressedVideo(_ savePath: String, _ sourceURL: URL) {
+        guard FFMPEGConverter.available else {
+            try? FileManager.default.moveItem(atPath: sourceURL.path, toPath: savePath)
+            print("The 'ffmpeg' isn't available.")
+            return
+        }
+
+        let convertPath = sourceURL.path.appending("-compressed.mp4")
+        exportDescription = "Compressed Video"
+        exportProgress = 0.0
+        FFMPEGConverter.convert(input: sourceURL.path, output: convertPath) { [weak self] result in
+            self?.exportProgress = 1.0
+            switch result {
+            case .success:
+                try? FileManager.default.moveItem(atPath: convertPath, toPath: savePath)
+            case .failure(let reason):
+                print(reason.localizedDescription)
+            }
+        }
+    }
+
+    /// Creates a filename for a video that ought to be unique
+    func makeVideoFilename() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd-HH-mm-ss"
+
+        let dateString = formatter.string(from: Date.now)
+
+        return "ControlRoom-\(dateString).mp4"
+    }
+}
--- ControlRoom/Simulator UI/ControlView.swift
+++ ControlRoom/Simulator UI/ControlView.swift
@@ -10,27 +10,14 @@ import SwiftUI
 
 /// The main tab view to control simulator settings.
 struct ControlView: View {
-    @ObservedObject var controller: SimulatorsController
+    /// Used to handle creating screenshots, videos, and GIFs.
+    @StateObject private var captureController = CaptureController()
 
-    /// The user's settings for capturing
-    @AppStorage("captureSettings") var captureSettings = CaptureSettings(imageFormat: .png, videoFormat: .h264, display: .internal, mask: .ignored)
+    /// Let's us watch the list of active simulators.
+    @ObservedObject var controller: SimulatorsController
 
     let simulator: Simulator
     let applications: [Application]
-
-    /// The currently active recording process, if it exists. We don't need to monitor this, just keep it alive.
-    @State private var recordingProcess: Process?
-
-    /// The name of the file we're writing to, used at first in a temporary directory then on the desktop.
-    @State private var recordingFilename = ""
-
-    /// The export format description to be shown while exporting
-    @State private var exportDescription = ""
-
-    /// Converting MP4 to GIF takes time, so this tracks the progress of the operation
-    @State private var exportProgress: CGFloat = 1.0
-
-    @State private var videoFormat = SimCtl.IO.VideoFormat.h264
 
     var body: some View {
         TabView {
@@ -43,44 +30,42 @@ struct ControlView: View {
         .disabled(simulator.state != .booted)
         .navigationSubtitle("\(simulator.name) – \(simulator.runtime?.name ?? "Unknown OS")")
         .toolbar {
-            Menu("Save \(captureSettings.imageFormat.rawValue.uppercased())") {
+            Menu("Save \(captureController.imageFormatString)") {
                 Button("Save as PNG") {
-                    takeScreenshot(format: .png)
+                    captureController.takeScreenshot(of: simulator, format: .png)
                 }
 
                 Button("Save as JPEG") {
-                    takeScreenshot(format: .jpeg)
+                    captureController.takeScreenshot(of: simulator, format: .jpeg)
                 }
 
                 Button("Save as TIFF") {
-                    takeScreenshot(format: .tiff)
+                    captureController.takeScreenshot(of: simulator, format: .tiff)
                 }
 
                 Button("Save as BMP") {
-                    takeScreenshot(format: .bmp)
+                    captureController.takeScreenshot(of: simulator, format: .bmp)
                 }
             } primaryAction: {
-                takeScreenshot(format: captureSettings.imageFormat)
+                captureController.takeScreenshot(of: simulator)
             }
 
-            if recordingProcess == nil {
-                Menu("Record \(captureSettings.videoFormat.name)") {
+            if captureController.recordingProcess == nil {
+                Menu("Record \(captureController.videoFormatString)") {
                     ForEach(SimCtl.IO.VideoFormat.all, id: \.self) { item in
                         if item == .divider {
                             Divider()
                         } else {
                             Button("Save as \(item.name)") {
-                                videoFormat = item
-                                startRecordingVideo()
+                                captureController.startRecordingVideo(of: simulator, format: item)
                             }
                         }
                     }
                 } primaryAction: {
-                    videoFormat = captureSettings.videoFormat
-                    startRecordingVideo()
+                    captureController.startRecordingVideo(of: simulator)
                 }
             } else {
-                Button("Stop Recording", action: stopRecordingVideo)
+                Button("Stop Recording", action: captureController.stopRecordingVideo)
             }
 
             if simulator.state != .booted {
@@ -101,115 +86,6 @@ struct ControlView: View {
     /// Terminates the current device.
     func shutdownDevice() {
         SimCtl.shutdown(simulator.udid)
-    }
-
-    /// Takes a screenshot of the device's current screen and saves it to the desktop.
-    func takeScreenshot(format: SimCtl.IO.ImageFormat) {
-        SimCtl.saveScreenshot(simulator.id, to: makeScreenshotFilename(format: format), type: format, display: captureSettings.display, with: captureSettings.mask)
-    }
-
-    /// Creates a filename for a screenshot that ought to be unique
-    func makeScreenshotFilename(format: SimCtl.IO.ImageFormat) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "y-MM-dd-HH-mm-ss"
-
-        let dateString = formatter.string(from: Date.now)
-
-        return "~/Desktop/ControlRoom-\(dateString).\(format.rawValue)"
-    }
-
-    /// Starts recording video of the device, saving it to the desktop.
-    func startRecordingVideo() {
-        recordingFilename = makeVideoFilename()
-
-        let tempPath = FileManager.default.temporaryDirectory.appendingPathComponent(recordingFilename).path
-
-        recordingProcess = SimCtl.startVideo(simulator.id, to: tempPath, type: .h264, display: captureSettings.display, with: captureSettings.mask)
-    }
-
-    func stopRecordingVideo() {
-        recordingProcess?.interrupt()
-        recordingProcess?.waitUntilExit()
-        recordingProcess = nil
-
-        let sourceURL = FileManager.default.temporaryDirectory.appendingPathComponent(recordingFilename)
-
-        let paths = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask)
-        let savePath = paths[0].appendingPathComponent(recordingFilename).path
-
-        let format = videoFormat.name
-
-        if format.hasPrefix("GIF") {
-            exportGif(format, savePath, sourceURL)
-        } else if format.contains("Compressed") {
-            exportCompressedVideo(savePath, sourceURL)
-        } else {
-            try? FileManager.default.moveItem(atPath: sourceURL.path, toPath: savePath)
-        }
-    }
-
-    /// Saves recorded video as a GIF-file
-    private func exportGif(_ format: String, _ savePath: String, _ sourceURL: URL) {
-        let size: CGFloat?
-
-        if format.contains("Small") {
-            size = 400
-        } else if format.contains("Medium") {
-            size = 800
-        } else if format.contains("Large") {
-            size = 1200
-        } else {
-            size = 1600
-        }
-
-        let gifExtension = savePath.replacingOccurrences(of: ".mp4", with: ".gif")
-
-        exportDescription = "GIF"
-
-        Task {
-            let result = try await sourceURL.convertToGIF(maxSize: size) { progress in
-                exportProgress = progress
-            }
-
-            switch result {
-            case .success(let gifURL):
-                try? FileManager.default.moveItem(atPath: gifURL.path, toPath: gifExtension)
-            case .failure(let reason):
-                print(reason.localizedDescription)
-            }
-        }
-    }
-
-    /// Compresses recorded video with `ffmpeg` before saving
-    private func exportCompressedVideo(_ savePath: String, _ sourceURL: URL) {
-        guard FFMPEGConverter.available else {
-            try? FileManager.default.moveItem(atPath: sourceURL.path, toPath: savePath)
-            print("The 'ffmpeg' isn't available.")
-            return
-        }
-
-        let convertPath = sourceURL.path.appending("-compressed.mp4")
-        exportDescription = "Compressed Video"
-        exportProgress = 0.0
-        FFMPEGConverter.convert(input: sourceURL.path, output: convertPath) { result in
-            exportProgress = 1.0
-            switch result {
-            case .success:
-                try? FileManager.default.moveItem(atPath: convertPath, toPath: savePath)
-            case .failure(let reason):
-                print(reason.localizedDescription)
-            }
-        }
-    }
-
-    /// Creates a filename for a video that ought to be unique
-    func makeVideoFilename() -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "y-MM-dd-HH-mm-ss"
-
-        let dateString = formatter.string(from: Date.now)
-
-        return "ControlRoom-\(dateString).mp4"
     }
 }
 
